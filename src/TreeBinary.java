4a69db46590c4ccaed18cd349408de32d1329774
Андрей К <varanoid1@gmail.com>
2023-08-24 23:05:56 +0700

    first commit

---
+++ src/TreeBinary.java
@@ -0,0 +1,104 @@
+public class TreeBinary {
+    Node root;
+    private Node addRecursive(Node current, int data) {
+        if (current == null) {
+            return new Node(data);
+        }
+        if (data < current.data) {
+            current.left = addRecursive(current.left, data);
+        } else if (data > current.data){
+            current.right = addRecursive(current.right, data);
+        } else {
+            return current;
+        }
+        return current;
+    }
+
+    public void add(int value) {
+        root = addRecursive(root, value);
+    }
+
+    private boolean containsNodeRecursive(Node current, int data) {
+        if (current == null) {
+            return false;
+        }
+        if (data == current.data) {
+            return true;
+        }
+        return data < current.data
+                ? containsNodeRecursive(current.left, data)
+                : containsNodeRecursive(current.right, data);
+    }
+
+    public boolean containsNode(int value) {
+        return containsNodeRecursive(root, value);
+    }
+
+    public void printTree() {
+        Node node = root;
+        recPrintTree(node);
+        System.out.println("____________");
+    }
+
+    private void recPrintTree(Node node) {
+        if (node.left != null) recPrintTree(node.left);
+        System.out.println(node.data);
+        if (node.right != null) recPrintTree(node.right);
+    }
+
+    private int findSmallValue(Node current) {
+        if (current.left == null) {
+            return root.data;
+        }
+        return findSmallValue(current.left);
+    }
+
+    private Node delElem(Node current, int value) {
+        if (current == null) {
+            return null;
+        }
+        if (value == current.data) {
+            if (current.left == null && current.right == null) {
+                return null;
+            }
+            if (current.left == null) {
+                return current.right;
+            }
+            if (current.right == null) {
+                return current.left;
+            }
+            int smallValue = findSmallValue(current.right);
+            current.data = smallValue;
+            current.right = delElem(current.right, smallValue);
+            return current;
+        }
+        if (value < current.data) {
+            current.left = delElem(current.left, value);
+            return current;
+        }
+        current.right = delElem(current.right, value);
+        return current;
+    }
+//      не смог сам придумать, как реализовать данный метод, увы
+
+    public void remove(int value) {
+        root = delElem(root, value);
+    }
+
+    public static void main(String[] args) {
+//        TreeBinary bt = new TreeBinary();
+//        bt.add(4);
+//        bt.add(2);
+//        bt.add(1);
+//        bt.add(3);
+//        bt.add(5);
+//        bt.add(6);
+//        bt.printTree();
+//        System.out.println(bt.containsNode(6));
+//        System.out.println(bt.containsNode(7));
+//        System.out.println(bt.containsNode(2));
+//        bt.remove(2);
+//        System.out.println(bt.containsNode(2));
+//        System.out.println(bt.containsNode(1));
+    }
+}
